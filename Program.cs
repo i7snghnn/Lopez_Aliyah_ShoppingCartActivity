fcafbed8331f84e490411a2e0cf392384fa2ecd1
aliyah lopez <al1yxhfiles@gmail.com>
2026-04-14 09:22:35 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
+# class 
+  using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class Product
+    {
+        public int Id;
+        public string Name;
+        public double Price;
+        public int Remaining_Stock;
+
+        public Product(int id, string name, double price, int stock)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Remaining_Stock = stock;
+        }
+
+       
+        public void DisplayProduct() 
+        {
+            string status = Remaining_Stock == 0 ? "OUT OF STOCK" : $"Stock: {Remaining_Stock}";
+            Console.WriteLine($"{Id}. {Name,-10} | ₱{Price,7:N2} | {status}");
+        }
+    }
+}
 
